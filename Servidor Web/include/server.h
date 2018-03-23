7b12936cb1e99e0d1e2964e68f2562b37ee58614
jairoM26 <jairomendezmartinez@.com>
2018-03-23 13:51:00 -0600

    Se agrega makefile

---
+++ Servidor Web/include/server.h
@@ -0,0 +1,7 @@
+#ifndef _SERVER_H
+#define _SERVER_H
+
+void server();
+char* sendImage();
+
+#endif
---
+++ Servidor Web/src/main.c
@@ -0,0 +1,7 @@
+#include "../lib/server.c"
+
+int main(int argc, char const *argv[])
+{
+    server();
+    return 0;
+}
